721c226b1ee996c0d0130975ddda0b967cd61fe0
JINAL-1 <59589615+JINAL-1@users.noreply.github.com>
2020-03-03 15:06:41 -0500

    Add files via upload

---
+++ A3.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+/*console.log(menu['soup']['price'])= 3.25
+			(menu.soup.price)
+			(menu.soup[price])*/
+const menus=[
+	{
+		title:'menu1',
+		soup:{
+					name:'minestrone',
+					price:2.95
+		},
+		salad:{
+					name:'greek salad',	
+					price:4.75
+		},
+		lighter_fare:{
+				    name:'vegetable biryani',
+					price:4.95
+	},
+		
+		entree:{
+				    name:'Breaded Chicken on a Bun',
+					price:5.95
+		}
+	},
+	
+	
+	{
+		title:'menu2',
+		soup:{
+					name:'Red Lentil Dal',
+					price:3.95
+		},
+		salad:{
+					name:'Julienne Salad',	
+					price:2.75
+		},
+		lighter_fare:{
+				    name:'Madras Chicken Salad ',
+					price:4.25
+	},
+		
+		entree:{
+				    name:'Fish & Chips',
+					price:4.95
+		}
+	},
+	
+	
+	},
+	
+	{
+		title:'menu3',
+		soup:{
+					name:'Spicy Squash & Pumpkin',
+					price:1.95
+		},
+		salad:{
+					name:'Mediterranean Salad',	
+					price:2.10
+		},
+		lighter_fare:{
+				    name:'Ham & Cheese Panini  ',
+					price:6.25
+	},
+		
+		entree:{
+				    name:'Butter Chicken with Rice Pilaf',
+					price:5.95
+		}
+	},
+	
+	{title:'menu4',
+		soup:{
+					name:'Tomato',
+					price:2.95
+		},
+		salad:{
+					name:'Caesar Salad',	
+					price:2.22
+		},
+		lighter_fare:{
+				    name:'Burger & Fries',
+					price:5.25
+	},
+		
+		entree:{
+				    name:'Beef & Broccoli with Fried Rice',
+					price:3.95
+		}
+	}
+]
+		
